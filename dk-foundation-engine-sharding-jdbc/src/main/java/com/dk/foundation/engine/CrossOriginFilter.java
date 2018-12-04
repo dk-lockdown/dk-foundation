f58020155fed9090b7d932ea19940e9d3cbf5058
liuxiaomin <dugukuangshao@qq.com>
2018-12-04 20:05:13 +0800

    添加跨域请求过滤器，解决跨域的时候Header不能传Authorization的问题

---
+++ dk-foundation-engine-sharding-jdbc/src/main/java/com/dk/foundation/engine/CrossOriginFilter.java
@@ -0,0 +1,29 @@
+package com.dk.foundation.engine;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CrossOriginFilter extends OncePerRequestFilter {
+    protected Logger logger = LoggerFactory.getLogger(CrossOriginFilter.class);
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getMethod().equals("OPTIONS")) {
+            logger.info("浏览器的预请求的处理..");
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,OPTIONS,DELETE");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,Authorization,token");
+            return;
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
