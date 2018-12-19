2bad8678eabc4b3fc571c3dbf8a8d418b4609526
liuxiaomin <dugukuangshao@qq.com>
2018-12-19 15:42:07 +0800

    redisson

--- dk-foundation-common/src/main/java/com/dk/foundation/common/RedisConfig.java
+++ dk-foundation-common/src/main/java/com/dk/foundation/common/RedisConfig.java
@@ -3,26 +3,19 @@ package com.dk.foundation.common;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.*;
-import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
-import redis.clients.jedis.JedisPoolConfig;
 
 /**
  * Created by duguk on 2018/2/1.
  */
 @Configuration
-@ConditionalOnProperty(name = "spring.redis.host")
+@ConditionalOnClass(RedisProperties.class)
 public class RedisConfig {
     final static Logger logger = LoggerFactory.getLogger(RedisConfig.class);
 
@@ -37,6 +30,7 @@ public class RedisConfig {
         redisTemplate.setHashValueSerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new StringRedisSerializer());
         redisTemplate.setConnectionFactory(factory);
+        redisTemplate.afterPropertiesSet();
         logger.info("-------------------- redisTemplate init ---------------------");
         return redisTemplate;
     }
---
+++ dk-foundation-common/src/main/java/com/dk/foundation/common/RedissonConfig.java
@@ -0,0 +1,69 @@
+package com.dk.foundation.common;
+
+import org.apache.commons.lang3.StringUtils;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.redisson.config.SentinelServersConfig;
+import org.redisson.config.SingleServerConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
+
+@Configuration
+@ConditionalOnClass(RedisProperties.class)
+public class RedissonConfig {
+
+    @Autowired
+    RedisProperties redisProperties;
+
+    @Bean
+    @ConditionalOnProperty(name="spring.redis.sentinel.master")
+    RedissonClient redissonSentinel() {
+        Config config = new Config();
+        List<String> sentinels= redisProperties.getSentinel().getNodes();
+        SentinelServersConfig serverConfig = config.useSentinelServers()
+                .setMasterName(redisProperties.getSentinel().getMaster())
+                .addSentinelAddress(sentinels.toArray(new String[sentinels.size()]))
+                .setDatabase(redisProperties.getDatabase());
+        if(StringUtils.isNotBlank(redisProperties.getPassword())) {
+            serverConfig.setPassword(redisProperties.getPassword());
+        }
+        if (redisProperties.getJedis().getPool() != null) {
+            serverConfig.setTimeout((int) redisProperties.getTimeout().getSeconds())
+                    .setMasterConnectionPoolSize(redisProperties.getJedis().getPool().getMaxActive())
+                    .setMasterConnectionMinimumIdleSize(redisProperties.getJedis().getPool().getMinIdle())
+                    .setSlaveConnectionPoolSize(redisProperties.getJedis().getPool().getMaxActive())
+                    .setSlaveConnectionMinimumIdleSize(redisProperties.getJedis().getPool().getMinIdle());
+        }
+        return Redisson.create(config);
+    }
+
+    @Bean
+    @ConditionalOnMissingBean(RedissonClient.class)
+    @ConditionalOnProperty(name="spring.redis.host")
+    RedissonClient redissonSingle() {
+        Config config = new Config();
+        SingleServerConfig serverConfig = config.useSingleServer()
+                .setAddress(redisProperties.getUrl())
+                .setDatabase(redisProperties.getDatabase());
+        if (StringUtils.isNotBlank(redisProperties.getPassword())) {
+            serverConfig.setPassword(redisProperties.getPassword());
+        }
+        if (redisProperties.getTimeout() != null) {
+            serverConfig.setTimeout((int) redisProperties.getTimeout().getSeconds());
+        }
+        if (redisProperties.getJedis().getPool() != null) {
+            serverConfig.setConnectionPoolSize(redisProperties.getJedis().getPool().getMaxActive())
+                    .setConnectionMinimumIdleSize(redisProperties.getJedis().getPool().getMinIdle());
+        }
+
+        return Redisson.create(config);
+    }
+}
