71da84f664553cbdfa2ed71def401b4d934fc361
liuxiaomin <dugukuangshao@qq.com>
2018-12-25 10:29:41 +0800

    修改redission加载条件

--- dk-foundation-common/src/main/java/com/dk/foundation/common/RedissonConfig.java
+++ dk-foundation-common/src/main/java/com/dk/foundation/common/RedissonConfig.java
@@ -8,7 +8,6 @@ import org.redisson.config.SentinelServersConfig;
 import org.redisson.config.SingleServerConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
