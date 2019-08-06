455b81ebf7b4b12d623748700f5fbb560e491aae
liuxiaomin <dugukuangshao@qq.com>
2019-08-06 14:45:44 +0800

    设置json序列化器

--- dk-foundation-engine/src/main/java/com/dk/foundation/engine/mvcconfigure/DefaultWebMvcConfigurerAdapter.java
+++ dk-foundation-engine/src/main/java/com/dk/foundation/engine/mvcconfigure/DefaultWebMvcConfigurerAdapter.java
@@ -3,16 +3,14 @@ package com.dk.foundation.engine.mvcconfigure;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.SerializeConfig;
 import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.serializer.ToStringSerializer;
 import com.alibaba.fastjson.support.config.FastJsonConfig;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import com.dk.foundation.engine.intercepter.CrossOriginInterceptor;
 import com.dk.foundation.engine.intercepter.IpInterceptor;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.stereotype.Component;
 import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
@@ -25,6 +23,7 @@ import java.util.List;
 /**
  * Created by duguk on 2018/1/5.
  */
+@Component
 public class DefaultWebMvcConfigurerAdapter extends WebMvcConfigurationSupport {
     @Value("${system.secure.open:false}")
     private String secureOpen;
