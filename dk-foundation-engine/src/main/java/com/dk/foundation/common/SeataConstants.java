17d719eb7843a200d940fb5e6d94066e60c7fb4f
liuxiaomin <dugukuangshao@qq.com>
2020-03-02 09:52:59 +0800

    避免xid传播

--- dk-foundation-engine/src/main/java/com/dk/foundation/common/SeataConstants.java
+++ dk-foundation-engine/src/main/java/com/dk/foundation/common/SeataConstants.java
@@ -1,6 +1,6 @@
 package com.dk.foundation.common;
 
 public class SeataConstants {
-    public static final String XID_HEADER="Seata-Xid";
-    public static final String TRANSACTION_MODE_HEADER="Seata-Transaction-Mode";
+    public static final String XID_HEADER="DK-Seata-Xid";
+    public static final String TRANSACTION_MODE_HEADER="DK-Seata-Transaction-Mode";
 }
